843c3f7c2aa440372c1368fdc69f5642e6cb61de
Guest User <Guest@Epicodus-5A.local>
2018-04-27 13:10:39 -0700

    add functional link for doctors with websites and return none if there is no website.

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ import './styles.css';
 
      let promise = new Promise(function(resolve, reject) {
        let request = new XMLHttpRequest();
-       let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_key=f048ff73c0df48da555dbb3b47493488&skip=0&limit=10&query=${symptom}`;
+       let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_key=f048ff73c0df48da555dbb3b47493488&skip=0&limit=4&query=${symptom}`;
        request.onload = function() {
          if (this.status === 200) {
            resolve(request.response);
@@ -27,15 +27,23 @@ import './styles.css';
 
      promise.then(function(response) {
        let body = JSON.parse(response);
-       let docList = body.data[0].practices;
+       for (let j=0; j<body.data.length; j++) {
+         let docList = body.data[j].practices;
          for(let i=0; i<docList.length; i++) {
            let name = docList[i].name;
-           let address = docList[i].visit_address.street;
+           let address = `${docList[i].visit_address.street}, ${docList[i].visit_address.city}`;
            let phone = docList[i].phones[0].number;
            let website = docList[i].website;
            let accepting = docList[i].accepts_new_patients;
-           $("#list").append(`<li>${name}<br>${address}<br>${phone}<br>${website}<br>${accepting}</li>`);
+           if (website === undefined) {
+             website = "none";
+           } else {
+             website = `<a href='${website}'>${website}</a>`;
+           }
+           let list = `<li>Name: ${name}<br>Address: ${address}<br>Phone: ${phone}<br>Website: ${website}<br>Accepting new patients: ${accepting}</li>`;
+           $("#list").append(list);
          }
+       }
       //  $("#name").text(`Name: ${display.name}`);
       //  $("#address").text(`Address: ${body.data[0].practices[0].visit_address.street}`);
       //  $("#phone").text(`Phone: ${body.data[0].practices[0].phones[0].number}`);

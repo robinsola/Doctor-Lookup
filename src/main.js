b6ea40a9fe4999744f7d6d8886d951fd3c3b265c
Guest User <Guest@Epicodus-5A.local>
2018-04-27 14:33:44 -0700

    add an alert if no doctors are available for submitted symptom and fix .env flie so api key works.

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ import './styles.css';
 
      let promise = new Promise(function(resolve, reject) {
        let request = new XMLHttpRequest();
-       let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_key=f048ff73c0df48da555dbb3b47493488&skip=0&limit=4&query=${symptom}`;
+       let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&skip=0&limit=4&user_key=${process.env.exports.apiKey}&query=${symptom}`;
        request.onload = function() {
          if (this.status === 200) {
            resolve(request.response);
@@ -24,9 +24,11 @@ import './styles.css';
        request.open("GET", url, true);
        request.send();
      });
-
      promise.then(function(response) {
        let body = JSON.parse(response);
+       if (body.data.length === 0) {
+         alert("There are currently no doctors available for that symptom, try a similar symptom.");
+       }
        for (let j=0; j<body.data.length; j++) {
          let docList = body.data[j].practices;
          for(let i=0; i<docList.length; i++) {
@@ -34,21 +36,21 @@ import './styles.css';
            let address = `${docList[i].visit_address.street}, ${docList[i].visit_address.city}`;
            let phone = docList[i].phones[0].number;
            let website = docList[i].website;
-           let accepting = docList[i].accepts_new_patients;
            if (website === undefined) {
              website = "none";
            } else {
-             website = `<a href='${website}'>${website}</a>`;
+             website = `<a href='${website}' target='blank'>${website}</a>`;
            }
-           let list = `<li>Name: ${name}<br>Address: ${address}<br>Phone: ${phone}<br>Website: ${website}<br>Accepting new patients: ${accepting}</li>`;
+           let accepting = docList[i].accepts_new_patients;
+           if (accepting === true) {
+             accepting = "yes";
+           } else {
+             accepting = "no";
+           }
+           let list = `<li><strong>${name}</strong><br>Address: ${address}<br>Phone: ${phone}<br>Website: ${website}<br>Accepting new patients: ${accepting}</li>`;
            $("#list").append(list);
          }
-       }
-      //  $("#name").text(`Name: ${display.name}`);
-      //  $("#address").text(`Address: ${body.data[0].practices[0].visit_address.street}`);
-      //  $("#phone").text(`Phone: ${body.data[0].practices[0].phones[0].number}`);
-      //  $("#website").text(`Website: ${body.data[0].practices[0].website}`);
-      //  $("#accepting").text(`Accepting new patients: ${body.data[0].practices[0].accepts_new_patients}`);
+        }
       }, function(error) {
         $("#showErrors").text(`There was an error processing your request: ${error.message}`);
       });
